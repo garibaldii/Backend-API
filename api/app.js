37699952ea5233e23bfbd3f7b391645e528d4185
Erick Oliveira <erick.sts.eo@gmail.com>
2024-06-06 20:13:24 -0300

    Swagger
    Add swagger e add configurações para chamar no app.js

--- api/app.js
+++ api/app.js
@@ -3,6 +3,9 @@ import connectDatabase from './src/database/db.js';
 import dotenv from 'dotenv'
 import cors from 'cors'
 
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+
 import professorRoute from './src/routes/professor.route.js';
 import userRoute from './src/routes/user.route.js';
 import courseRoute from './src/routes/course.route.js';
@@ -21,6 +24,18 @@ app.use('/professors', professorRoute);
 app.use('/user', userRoute);
 app.use('/course', courseRoute);
 app.use('/auth', authRoute);
+
+
+
+// Configuração do Swagger
+try {
+    const jsonData = fs.readFileSync('./swagger_output.json', 'utf8');
+    const swaggerDocument = JSON.parse(jsonData);
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    console.log('Swagger configurado com sucesso!');
+} catch (error) {
+    console.error('Erro ao configurar o Swagger:', error);
+}
 
 
 app.listen(port, () => console.log(`App rodando na porta ${port} 💻`));
