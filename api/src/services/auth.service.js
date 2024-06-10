57e31fbdd76837fcfe43b06bc0513145e0a00fbe
Leon Fagundes <leonfagundes7@gmail.com>
2024-06-10 18:55:28 -0300

    add Testes

--- api/src/services/auth.service.js
+++ api/src/services/auth.service.js
@@ -1,20 +1,9 @@
 import userModel from '../model/user.model.js';
 import jwt from 'jsonwebtoken';
 
-const loginService = (email) => {
-    try {
-        userModel.findOne({ email: email }).select('+password')
-    } catch (err) {
-        return "Erro ao logar, problema no Banco de Dados"
-    }
+const loginService = (email) => userModel.findOne({ email: email }).select('+password')
 
-}
+const genereteToken = (id, email) => jwt.sign({id: id, email: email}, process.env.SECRET_JWT, {expiresIn: 86400});
 
-const genereteToken = (id, email) => {
-    try {
-        jwt.sign({id: id, email: email}, process.env.SECRET_JWT, {expiresIn: 86400});
-    } catch (err) {
-        return "Erro no Banco de Dados"
-    }
-}
+
 export { loginService, genereteToken };
