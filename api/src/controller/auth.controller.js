eedc548b4ee3b81dcea33443b86f7c0c1f3a3183
erick-sts <erick.sts.eo@gmail.com>
2024-06-10 11:28:34 -0300

    Correcoes
    Respostas do modal com message do backend

--- api/src/controller/auth.controller.js
+++ api/src/controller/auth.controller.js
@@ -6,17 +6,17 @@ const loginController = async (req, res) => {
     try {
         const user = await loginService(email);
         if (!user) {
-            return res.status(400).send({ msg: 'E-mail ou senha inválidas!' });
+            return res.status(400).send({ msg: 'E-mail  inválido!' });
         }
 
         const passwordIsValid = bcrypt.compareSync(password, user.password);
         if (!passwordIsValid) {
-            return res.status(400).send({ msg: 'E-mail ou senha inválidas!' });
+            return res.status(400).send({ msg: 'Senha inválida!' });
         }
 
         const token = genereteToken(user.id, user.email);
 
-        return res.status(200).send({ msg: 'Logado! ✅', token });
+        return res.status(200).send({ msg: 'Bem vindo!', token });
     } catch (err) {
         console.error('Erro no servidor:', err.message);
         return res.status(500).send({ msg: err.message });
